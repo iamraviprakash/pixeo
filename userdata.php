36055265373977736493c6cbe1b12240b0efc46a
Ravi Prakash <ravi.p15@iiits.in>
2017-05-14 23:05:03 +0530

    Update userdata.php

--- userdata.php
+++ userdata.php
@@ -29,57 +29,57 @@
 		$len=strpos($uname,';');
 		if($len>0)
 			$uname=substr($uname,0,$len);
-		$sql="select User_Id from GUser where User_Name='$uname'";
+		$sql="select user_id from GUser where user_username='$uname'";
 		$result=$conn->query($sql);
 		$row=$result->fetch_assoc();
-		$uid=$row['User_Id'];
+		$uid=$row['user_id'];
 		if($_GET['value']=='like')
 		{
-			$sql="select * from Liked_Videos where Video_Id=$vid and User_Id=$uid";
+			$sql="select * from liked_videos where video_id=$vid and user_id=$uid";
 			$result=$conn->query($sql);
 			if($result->num_rows>0)
 			{
 				//return same value
 				$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
-				$conn->query("update Videos set Likes=Likes-1 where Video_Id=$vid");
-				$conn->query("Delete from Liked_Videos where Video_Id=$vid and User_Id=$uid");
+				$conn->query("update videos set likes=likes-1 where video_id=$vid");
+				$conn->query("Delete from liked_videos where video_id=$vid and user_id=$uid");
 				$conn->commit();
-				$sql="select Likes,Dislikes from Videos where Video_Id=$vid";
+				$sql="select likes,dislikes from videos where video_id=$vid";
 				$result=$conn->query($sql);
 				$row=$result->fetch_assoc();
-				$str=$row['Likes'].",".$row['Dislikes'];
+				$str=$row['likes'].",".$row['dislikes'];
 				echo $str;
 			}
 			else
 			{
-				$sql="select * from Disliked_Videos where Video_Id=$vid and User_Id=$uid";
+				$sql="select * from disliked_videos where video_id=$vid and user_id=$uid";
 				$result=$conn->query($sql);
 				if($result->num_rows>0)
 				{
 					//update like and dislike remove from dislike add to like
 					$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
-					$conn->query("update Videos set Dislikes=Dislikes-1 where Video_Id=$vid");
-					$conn->query("update Videos set Likes=Likes+1 where Video_Id=$vid");
-					$conn->query("Delete from Disliked_Videos where Video_Id=$vid and User_Id=$uid");
-					$conn->query("Insert into Liked_Videos(Video_Id,User_Id) values($vid,$uid)");
+					$conn->query("update videos set dislikes=dislikes-1 where video_id=$vid");
+					$conn->query("update videos set likes=likes+1 where video_id=$vid");
+					$conn->query("Delete from disliked_videos where video_id=$vid and user_id=$uid");
+					$conn->query("Insert into liked_videos(video_id,user_id) values($vid,$uid)");
 					$conn->commit();
-					$sql="select Likes,Dislikes from Videos where Video_Id=$vid";
+					$sql="select likes,dislikes from videos where video_id=$vid";
 					$result=$conn->query($sql);
 					$row=$result->fetch_assoc();
-					$str=$row['Likes'].",".$row['Dislikes'];
+					$str=$row['likes'].",".$row['dislikes'];
 					echo $str;
 				}
 				else
 				{
 					//update like
 					$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
-					$conn->query("update Videos set Likes=Likes+1 where Video_Id=$vid");
-					$conn->query("Insert into Liked_Videos(Video_Id,User_Id) values($vid,$uid)");
+					$conn->query("update videos set likes=likes+1 where video_id=$vid");
+					$conn->query("Insert into liked_videos(video_id,user_id) values($vid,$uid)");
 					$conn->commit();
-					$sql="select Likes,Dislikes from Videos where Video_Id=$vid";
+					$sql="select likes,dislikes from videos where video_id=$vid";
 					$result=$conn->query($sql);
 					$row=$result->fetch_assoc();
-					$str=$row['Likes'].",".$row['Dislikes'];
+					$str=$row['likes'].",".$row['dislikes'];
 					echo $str;
 				}
 			}
@@ -87,51 +87,51 @@
 		}
 		else if($_GET['value']=='dislike')
 		{
-			$sql="select * from Disliked_Videos where Video_Id=$vid and User_Id=$uid";
+			$sql="select * from disliked_videos where video_id=$vid and user_id=$uid";
 			$result=$conn->query($sql);
 			if($result->num_rows>0)
 			{
 				//return same value
 				$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
-				$conn->query("update Videos set Dislikes=Dislikes-1 where Video_Id=$vid");
-				$conn->query("Delete from Disliked_Videos where Video_Id=$vid and User_Id=$uid");
+				$conn->query("update videos set dislikes=dislikes-1 where video_id=$vid");
+				$conn->query("Delete from disliked_videos where video_id=$vid and user_id=$uid");
 				$conn->commit();
-				$sql="select Likes,Dislikes from Videos where Video_Id=$vid";
+				$sql="select likes,dislikes from videos where video_id=$vid";
 				$result=$conn->query($sql);
 				$row=$result->fetch_assoc();
-				$str=$row['Likes'].",".$row['Dislikes'];
+				$str=$row['likes'].",".$row['dislikes'];
 				echo $str;
 			}
 			else
 			{
-				$sql="select * from Liked_Videos where Video_Id=$vid and User_Id=$uid";
+				$sql="select * from liked_videos where video_id=$vid and user_id=$uid";
 				$result=$conn->query($sql);
 				if($result->num_rows>0)
 				{
 						//update like and dislike remove from dislike add to like
 					$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
-					$conn->query("update Videos set Likes=Likes-1 where Video_Id=$vid");
-					$conn->query("update Videos set Dislikes=Dislikes+1 where Video_Id=$vid");
-					$conn->query("Delete from Liked_Videos where Video_Id=$vid and User_Id=$uid");
-					$conn->query("Insert into Disliked_Videos(Video_Id,User_Id) values($vid,$uid)");
+					$conn->query("update videos set likes=likes-1 where video_id=$vid");
+					$conn->query("update videos set dislikes=dislikes+1 where video_id=$vid");
+					$conn->query("Delete from liked_videos where video_id=$vid and user_id=$uid");
+					$conn->query("Insert into disliked_videos(video_id,user_id) values($vid,$uid)");
 					$conn->commit();
-					$sql="select Likes,Dislikes from Videos where Video_Id=$vid";
+					$sql="select likes,dislikes from videos where video_id=$vid";
 					$result=$conn->query($sql);
 					$row=$result->fetch_assoc();
-					$str=$row['Likes'].",".$row['Dislikes'];
+					$str=$row['likes'].",".$row['dislikes'];
 					echo $str;
 				}
 				else
 				{
 					//update like
 					$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
-					$conn->query("update Videos set Dislikes=Dislikes+1 where Video_Id=$vid");
-					$conn->query("Insert into Disliked_Videos(Video_Id,User_Id) values($vid,$uid)");
+					$conn->query("update videos set dislikes=dislikes+1 where video_id=$vid");
+					$conn->query("Insert into disliked_videos(video_id,user_id) values($vid,$uid)");
 					$conn->commit();
-					$sql="select Likes,Dislikes from Videos where Video_Id=$vid";
+					$sql="select likes,dislikes from videos where video_id=$vid";
 					$result=$conn->query($sql);
 					$row=$result->fetch_assoc();
-					$str=$row['Likes'].",".$row['Dislikes'];
+					$str=$row['likes'].",".$row['dislikes'];
 					echo $str;
 				}
 			}
@@ -140,12 +140,29 @@
 		else if($_GET['value']=='comment')
 		{
 			$text=$_GET['text'];
-			$sql="Insert into Comments(Text,Video_Id,User_Id) values('$text','$vid','$uid');";
+			$sql="Insert into comments(text,video_id,user_id) values('$text','$vid','$uid');";
 			$conn->query($sql);
-			$sql="select Count(User_Id) as Total_Count from Comments where Video_Id=$vid";
+			$sql="select Count(user_id) as Total_Count from comments where video_id=$vid";
 			$result=$conn->query($sql);
 			$row=$result->fetch_assoc();
 			$str=$row['Total_Count'];
+			echo $str;
+		}
+		else if($_GET['value']=='views')
+		{
+			$sql="select * from history where video_id=$vid and user_id=$uid";
+			$result=$conn->query($sql);
+			if ($result->num_rows == 0) 
+			{
+				$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
+				$conn->query("update videos set views=views+1 where video_id=$vid");
+				$conn->query("Insert into history(video_id,user_id) values($vid,$uid)");
+				$conn->commit();
+			}
+			$sql="select views from videos where video_id=$vid";
+			$result=$conn->query($sql);
+			$row=$result->fetch_assoc();
+			$str=$row['views'];
 			echo $str;
 		}
 	}
